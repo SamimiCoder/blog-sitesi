e624f0d09a89eae0e9b9a1a8f8b0feffcc956d98
Berkay Saraç <40615491+BerkaySarac-hub@users.noreply.github.com>
2022-08-26 20:23:53 +0300

    validator

---
+++ src/middlewares/validation_middleware.js
@@ -0,0 +1,34 @@
+const { body } = require("express-validator");
+
+const validateNewUser = () => {
+  return [
+    body("email")
+      .trim()
+      .isEmail()
+      .withMessage("lütfen geçerli bir email adresi giriniz"),
+    body("sifre")
+      .trim()
+      .isLength({ min: 6 })
+      .withMessage("şifreniz en az 6 karakter olmalı")
+      .isLength({ max: 20 })
+      .withMessage("şifreniz en fazla 20 karakter olmalı"),
+    body("isim")
+      .trim()
+      .isLength({ min: 3 })
+      .withMessage("isminiz en az 3 karakter olmalı"),
+    body("soyad")
+      .trim()
+      .isLength({ min: 2 })
+      .withMessage("soyad en az 2 karakter olmalı"),
+    body("resifre")
+      .trim()
+      .custom((value, { req }) => {
+        if (value !== req.body.sifre) {
+          throw new Error("Şifreleriniz uyuşmuyor");
+        }
+      }),
+  ];
+};
+module.exports = {
+  validateNewUser,
+};
--- src/routers/auth_router.js
+++ src/routers/auth_router.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
 const auth_controller = require("../controllers/auth_controller");
+const validator = require("../middlewares/validation_middleware");
 router.get("/login", auth_controller.loginFormunuGoster);
 router.post("/login", auth_controller.login);
 router.get("/register", auth_controller.registerFormunuGoster);
-router.post("/register", auth_controller.register);
+router.post("/register", validator.validateNewUser, auth_controller.register);
 router.get("/forgot-password", auth_controller.forgotPasswordFormunuGoster);
 router.post("/forgot-password", auth_controller.forgotPassword);
 module.exports = router;
